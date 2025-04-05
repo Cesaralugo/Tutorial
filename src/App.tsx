a7edd45160aba5570c85b55b8a187943c1f5ed5e
Cesaralugo <cesaralugo31@gmail.com>
2025-04-05 18:35:39 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import LocationDisplay from "./LocationDisplay";
 
 const client = generateClient<Schema>();
 
 function App() {
+
+  const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -17,22 +21,55 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
     <main>
+
+ 
+
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+ 
       <h1>My todos</h1>
+ 
       <button onClick={createTodo}>+ new</button>
+ 
       <ul>
+ 
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+ 
+          <li
+ 
+            onClick={() => deleteTodo(todo.id)}
+ 
+            key={todo.id}>{todo.content}</li>
+ 
         ))}
+ 
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+        <h2>Location</h2>
+        <LocationDisplay />
       </div>
+      <div>
+ 
+        🥳 App successfully hosted. Try creating a new todo.
+ 
+        <br />
+ 
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+ 
+          Review next step of this tutorial.
+ 
+        </a>
+ 
+      </div>
+ 
+      <button onClick={signOut}>Sign out</button>
+ 
     </main>
   );
 }
