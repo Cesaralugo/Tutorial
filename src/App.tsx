a3c51e8fba06ee7e46b442e3bc7ad0e3580c9655
Juanma-phib <juanmanuelbornacellym@gmail.com>
2025-04-05 19:42:14 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,19 @@
-import { useEffect, useState } from "react";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
 import LocationDisplay from "./LocationDisplay";
 
-const client = generateClient<Schema>();
-
 function App() {
-
-  const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-    
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
+  const { user, signOut } = useAuthenticator(); // ✅ Ya tienes el usuario logueado
 
   return (
     <main>
-
- 
-
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
- 
-      <h1>My todos</h1>
- 
-      <button onClick={createTodo}>+ new</button>
- 
-      <ul>
- 
-        {todos.map((todo) => (
- 
-          <li
- 
-            onClick={() => deleteTodo(todo.id)}
- 
-            key={todo.id}>{todo.content}</li>
- 
-        ))}
- 
-      </ul>
+      <h1>Bienvenido, {user?.signInDetails?.loginId}</h1>
+      
       <div>
-        <h2>Location</h2>
-        <LocationDisplay />
+        <h2>Obtener Ubicación</h2>
+        <LocationDisplay user={user} /> {/* Pasamos el usuario como prop */}
       </div>
-      <div>
- 
-        🥳 App successfully hosted. Try creating a new todo.
- 
-        <br />
- 
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
- 
-          Review next step of this tutorial.
- 
-        </a>
- 
-      </div>
- 
-      <button onClick={signOut}>Sign out</button>
- 
+
+      <button onClick={signOut}>Cerrar sesión</button>
     </main>
   );
 }
