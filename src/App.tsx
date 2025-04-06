0be51fb0edf2340f029b464f3c0617a82aa2c5c7
Juanma-phib <juanmanuelbornacellym@gmail.com>
2025-04-05 20:39:08 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,25 @@
 import { useEffect, useState } from "react";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
 import LocationDisplay from "./LocationDisplay";
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
 
-const client = generateClient<Schema>();
+Amplify.configure(awsconfig);
 
 function App() {
-
   const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-    
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
 
   return (
     <main>
-
- 
-
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
- 
-      <h1>My todos</h1>
- 
-      <button onClick={createTodo}>+ new</button>
- 
-      <ul>
- 
-        {todos.map((todo) => (
- 
-          <li
- 
-            onClick={() => deleteTodo(todo.id)}
- 
-            key={todo.id}>{todo.content}</li>
- 
-        ))}
- 
-      </ul>
-      <div>
-        <h2>Location</h2>
-        <LocationDisplay />
+      <h1>Bienvenido, {user?.signInDetails?.loginId}</h1>
+      
+      {/* Sección de Ubicación */}
+      <div style={{ margin: '20px 0', padding: '20px', border: '1px solid #ddd', borderRadius: '8px' }}>
+        <h2>Ubicación Actual</h2>
+        <LocationDisplay user={user} />
       </div>
-      <div>
- 
-        🥳 App successfully hosted. Try creating a new todo.
- 
-        <br />
- 
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
- 
-          Review next step of this tutorial.
- 
-        </a>
- 
-      </div>
- 
-      <button onClick={signOut}>Sign out</button>
- 
+
+      <button onClick={signOut}>Cerrar sesión</button>
     </main>
   );
 }
